671a2d8e00635f0c11618019f6784444843640dc
Hack Reactor Student <student@hackreactor.com>
2017-07-05 16:02:05 -0700

    functional queue and stack completed

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,12 +7,27 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    // Add a string to the back of the queue
+    storage[someInstance.size() + 1] = value;
   };
 
   someInstance.dequeue = function() {
+    // Remove and return the string at the front of the queue
+    var result = storage['1'];
+    delete storage['1'];
+    for (var keys in storage) {
+      var newKey = keys - 1;
+      storage[newKey] = storage[keys];
+      delete storage[keys];
+    }
+    return result;
   };
 
   someInstance.size = function() {
+    // Return the number of items in the queue
+    var sizeArray = Object.keys(storage);
+    return sizeArray.length;
+
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,26 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    //Add a string to the top of the stack
+    // the key will size + 1;
+    storage[someInstance.size() + 1] = value;
   };
 
   someInstance.pop = function() {
+    // Remove and return the string on the top of the stack
+    // return the last element && remove it
+    var result = storage[someInstance.size()];
+    delete storage[someInstance.size()];
+    return result;
+
   };
 
   someInstance.size = function() {
+    // Return the number of items on the stack
+    // call the method keys on a objs => array;
+    var sizeArray = Object.keys(storage);
+    return sizeArray.length;
+
   };
 
   return someInstance;

36fa5c0dadf520bcb6c4942d6795e8ae07148d0e
Hack Reactor Student <student@hackreactor.com>
2017-07-06 16:08:44 -0700

    delete comments when using RegEx

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,27 +1,27 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
-  // Use the keyword this in your methods
-
-  var instanceObj = {
-    storage: {}
-  };
+  var instanceObj = {};
+  instanceObj.storage = {};
   _.extend(instanceObj, queueMethods);
+  
   return instanceObj;
 };
 
 var queueMethods = {
 
-  enqueue: function(value) { this.storage[this.size() + 1] = value; },
+  enqueue: function(value) { 
+    
+    this.storage[this.size() + 1] = value; 
+  },
 
   dequeue: function() {
+
     var result = this.storage['1'];
     delete this.storage['1'];
     for (var keys in this.storage) {
       var newKey = keys - 1;
       this.storage[newKey] = this.storage[keys];
       delete this.storage[keys];
-    } 
+    }
     return result;
   },
 

3839fcd434979374462d71b794a65d3f1d00a91b
Hack Reactor Student <student@hackreactor.com>
2017-07-05 20:47:28 -0700

    functional-shared queue and stack completed w/weird test

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,37 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  // Use the keyword this in your methods
+
+  var instanceObj = {
+    storage: {}
+  };
+  _.extend(instanceObj, queueMethods);
+  return instanceObj;
 };
 
-var queueMethods = {};
+var queueMethods = {
+
+  enqueue: function(value) { this.storage[this.size() + 1] = value; },
+
+  dequeue: function() {
+    var result = this.storage['1'];
+    delete this.storage['1'];
+    for (var keys in this.storage) {
+      var newKey = keys - 1;
+      this.storage[newKey] = this.storage[keys];
+      delete this.storage[keys];
+    } 
+    return result;
+  },
+
+  size: function() {
+    return Object.keys(this.storage).length;
+  },
+
+  //storage: {}
+};
+
+
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,30 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  // Use the keyword this in your methods
+  var instanceObj = {
+    storage: {}
+  };
+  _.extend(instanceObj, stackMethods);
+  return instanceObj;
 };
 
-var stackMethods = {};
+var stackMethods = {
+
+  push: function(value) {
+    this.storage[this.size() + 1] = value;
+  },
+
+  pop: function() {
+    var result = this.storage[this.size()];
+    delete this.storage[this.size()];
+    return result;
+  },
+
+  size: function() {
+    return Object.keys(this.storage).length;
+  }
+
+};
 
 

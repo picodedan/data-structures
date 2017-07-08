f30df0909dc79e0815953d44f02bdd4768cf7882
Hack Reactor Student <student@hackreactor.com>
2017-07-08 16:49:31 -0700

    in process

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,41 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  //get bucket at index
+  var bucket = this._storage.get(index) || [];
+  //create tuple of k,v
+  var tuple = [k, v];
+  //add  tuple to bucket
+  bucket.push(tuple);  
+  //set bucket in storage
+  this._storage.set(index, bucket);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  //get bucket at index
+  var bucket = this._storage.get(index);
+  // iterate through bucket to find k 
+  for (var i = 0; i < bucket.length; i++) {
+    if (bucket[i][0] === k) {
+      //return value 
+      return bucket[i][1];
+    }    
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  // get bucket by index
+  var bucket = this._storage.get(index);
+  // iterate through bucket to find k
+  for (var i = 0; i < bucket.length; i++) {
+    //if k is found
+    if (bucket[i][0] === k) {
+      //delete the bucket elemet (splice(i, 1))
+      bucket.splice(i, 1);
+    }
+  }    
 };
 
 
@@ -22,5 +49,9 @@ HashTable.prototype.remove = function(k) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+var obj = {
+  some: value
+};
 
 
